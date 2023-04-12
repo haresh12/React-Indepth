d01e5e9fc674b5a6e9c9a93e8ae9b79b5dc0ea8f
haresh <lakhwaniharesh134@gmail.com>
2023-04-13 03:12:00 +0530

    Day2 started with igniting app basic

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
 
+import React from "react";
+import ReactDOM  from "react-dom/client";
 
 let head = React.createElement('h1',{},"Learning In Depth React");
 let root = ReactDOM.createRoot(document.getElementById('root'));
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,5 @@
+let head = React.createElement("h1", {}, "Learning In Depth React");
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(head);
+
+//# sourceMappingURL=index.6bd02f5a.js.map
