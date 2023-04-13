5a6b26bb3ad2dbc47fddcd9210cac0689dcc6dfe
haresh <lakhwaniharesh134@gmail.com>
2023-04-14 02:32:37 +0530

    Day 3 igniting app learn about parcel

--- dist/index.6bd02f5a.js
+++
@@ -1,5 +0,0 @@
-let head = React.createElement("h1", {}, "Learning In Depth React");
-let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(head);
-
-//# sourceMappingURL=index.6bd02f5a.js.map
